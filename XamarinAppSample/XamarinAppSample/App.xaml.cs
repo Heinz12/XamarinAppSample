b60877071f0c479fc7bffbb9a29f0f5b516d7a8d
Heinz Holzer <holzerheinz@hotmail.com>
2020-07-18 09:26:53 +0200

    Add project files.

---
+++ XamarinAppSample/XamarinAppSample/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using XamarinAppSample.Services;
+using XamarinAppSample.Views;
+
+namespace XamarinAppSample
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ XamarinAppSample/XamarinAppSample/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using XamarinAppSample.Models;
+
+namespace XamarinAppSample.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
